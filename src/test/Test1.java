de5fdc173502e14f391db3e88822a25e30106b34
kulmeet.sawhney <kulmeetkaur54@gmail.com>
2020-05-04 22:16:14 +0530

    first commit

---
+++ src/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+public class Test1 {	
+		
+		@Test
+		public void NativeApp()	
+		{
+			
+			System.out.println("NativeApp");
+		}
+			
+		
+
+	}
+
+
